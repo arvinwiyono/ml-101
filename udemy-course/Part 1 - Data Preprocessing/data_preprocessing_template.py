9afab008d13a2bdd5bbc2f056cb90211df30f06f
Arvin Wiyono <arvinwiyono@gmail.com>
2017-07-07 13:28:19 +0700

    Feature scaling

--- udemy-course/Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ udemy-course/Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -39,3 +39,11 @@ y = le_y.fit_transform(y)
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+# Feature scaling
+# Put the values of age and salary on the same scale
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+scaled_X_train = sc_X.fit_transform(X)
+# already fitted, so just use transform()
+scaled_X_test = sc_X.transform(X_test)
+
