47995ff23d8d1192ed829fece8fe0a448d4819c8
Arvin Wiyono <arvinwiyono@gmail.com>
2017-07-07 13:13:13 +0700

    Split between train and test data

--- udemy-course/Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ udemy-course/Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -1,19 +1,41 @@
+# Data Preprocessing
+
+# Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# importing the dataset
-data = pd.read_csv('Data.csv')
+# Importing the dataset
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 3].values
 
-# experiment
-print(data.iloc[1:4, :-2]) # this returns the dataframe
-print(data.iloc[1:4, :-2].values) # this returns a numpy array
-print(type(data.iloc[1:4, :-2].values))
+# Taking care of missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
 
-# separate the independent variables (features)
-X = data.iloc[:, :-1].values
-print(X)
+# Categorical variables are: Country and Purchased
+from sklearn.preprocessing import LabelEncoder
+le_X = LabelEncoder()
+X[:, 0] = le_X.fit_transform(X[:, 0])
 
-# the labels
-Y = data.iloc[:, -1].values
-print(Y)
+# however this leads to another problem
+# the machine learning model can think that Spain is higher than France,
+# Germany is smaller than Spain, etc.
+# So there should not be a relational order
+
+# A better way is using one hot encoding or dummy encoding
+from sklearn.preprocessing import OneHotEncoder
+enc = OneHotEncoder(categorical_features = [0]) # specify the column that we one-hot-encode
+X = enc.fit_transform(X).toarray() # can only take numerical inputs
+
+# For the Purchased, we just need to use the Label Encoder as it is only either 0 or 1
+le_y = LabelEncoder()
+y = le_y.fit_transform(y)
+
+# Splitting the dataset into the Training and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
