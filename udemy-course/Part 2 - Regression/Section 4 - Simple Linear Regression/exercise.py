d96ded41803f342429c3c4407210ba352c57f294
Arvin Wiyono <arvinwiyono@gmail.com>
2017-07-08 01:07:19 +0700

    Simple linear regression completed. Can make predictions on training and test datasets.

--- udemy-course/Part 2 - Regression/Section 4 - Simple Linear Regression/exercise.py
+++ udemy-course/Part 2 - Regression/Section 4 - Simple Linear Regression/exercise.py
@@ -8,7 +8,7 @@ X = dataset.iloc[:,:-1].values # needs to be a matrix
 Y = dataset['Salary'].values
 
 from sklearn.model_selection import train_test_split
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 1.0/3, random_state=42)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 1.0/3, random_state=0)
 
 # data is well preprocessed now
 
@@ -20,3 +20,24 @@ regressor.fit(X_train, Y_train)
 #regressor_cv = LinearRegression()
 #cross_val_score(regressor_cv, X_train, Y_train, cv=4)
 
+# Predicting the test results
+Y_pred = regressor.predict(X_test)
+coeff_r2 = regressor.score(X_test, Y_test)
+print("Coefficient R2 is {0:>5.5f}".format(coeff_r2))
+
+# Visualising the training set results
+plt.scatter(X_train, Y_train, color='red')
+plt.plot(X_train, regressor.predict(X_train), color='green')
+plt.title('Salary vs Experience (Training Set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.savefig('salary-vs-experience-training.png', format='png', dpi=350)
+plt.show()
+
+plt.scatter(X_test, Y_test, color='red')
+plt.plot(X_train, regressor.predict(X_train), color='green')
+plt.title('Salary vs Experience (Test Set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.savefig('salary-vs-experience-testing.png', format='png', dpi=350)
+plt.show()
