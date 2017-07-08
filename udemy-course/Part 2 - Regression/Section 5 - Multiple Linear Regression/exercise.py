3ced8a68cd659d643756887c472225389ae07411
Arvin Wiyono <arvinwiyono@gmail.com>
2017-07-09 00:38:38 +0700

    Fit linear regression to the training dataset

--- udemy-course/Part 2 - Regression/Section 5 - Multiple Linear Regression/exercise.py
+++ udemy-course/Part 2 - Regression/Section 5 - Multiple Linear Regression/exercise.py
@@ -8,17 +8,33 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('50_Startups.csv')
 X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
+y = dataset.iloc[:, -1].values # the last column (Profit)
+
+# Data encoding
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+le = LabelEncoder()
+X[:, -1] = le.fit_transform(X[:,-1]) # labelling the State
+enc = OneHotEncoder(categorical_features=[3])
+X = enc.fit_transform(X).toarray()
+
+# Avoiding the dummy variable trap
+X = X[:, 1:] # ignoring the first dummy column :)
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-# Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
+# Fitting Multiple Linear Regression to the Training Set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
 
+y_pred = regressor.predict(X_test)
+
+score = regressor.score(X_test, y_test)
+print(f"Score is {score}")
+
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+mae = mean_absolute_error(y_test, y_pred)
+mse = mean_squared_error(y_test, y_pred)
+print('Predicted vs Real errors:', mae, '||', mse)
