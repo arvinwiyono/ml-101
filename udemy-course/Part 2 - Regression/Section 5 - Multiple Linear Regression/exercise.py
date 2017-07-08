a543eddde03b3455e0e84a31f93c266234b26984
Arvin Wiyono <arvinwiyono@gmail.com>
2017-07-09 01:32:06 +0700

    Completed Backward Elimination and built optimal regressor

--- udemy-course/Part 2 - Regression/Section 5 - Multiple Linear Regression/exercise.py
+++ udemy-course/Part 2 - Regression/Section 5 - Multiple Linear Regression/exercise.py
@@ -1,4 +1,4 @@
-# Data Preprocessing Template
+# Multivariate Linear Regression with Backward Elimination
 
 # Importing the libraries
 import numpy as np
@@ -47,3 +47,46 @@ import statsmodels.formula.api as smf
 # this adds the ones at the beginning
 X = np.append(arr = np.ones((50,1)), values = X, axis = 1)
 
+# Start Backward Elimination
+X_opt = X[:,[0,1,2,3,4,5]] # original optimal matrix
+regressor_ols = smf.OLS(endog=y, exog=X_opt).fit()
+print(regressor_ols.summary())
+
+# Remove first dummy var, which has a p-val = 0.990 > 0.05
+X_opt = X[:,[0,1,3,4,5]] # original optimal matrix
+regressor_ols = smf.OLS(endog=y, exog=X_opt).fit()
+print(regressor_ols.summary())
+print("\n")
+
+# Remove second dummy var, which has a p-val = 0.940 > 0.05
+X_opt = X[:,[0,3,4,5]] # original optimal matrix
+regressor_ols = smf.OLS(endog=y, exog=X_opt).fit()
+print(regressor_ols.summary())
+print("\n")
+
+# Remove Administration, which has a p-val = 0.602 > 0.05
+X_opt = X[:,[0,3,5]] # original optimal matrix
+regressor_ols = smf.OLS(endog=y, exog=X_opt).fit()
+print(regressor_ols.summary())
+print("\n")
+
+# Remove Marketing Spend, which has a p-val = 0.06 > 0.05
+X_opt = X[:,[0,3]] # original optimal matrix
+regressor_ols = smf.OLS(endog=y, exog=X_opt).fit()
+print(regressor_ols.summary())
+print("\n")
+
+# Therefore, in this case, the significant predictor is only the R&D spend
+# Now build the optimal regressor
+print("------------OPTIMAL MODEL---------------")
+X_train, X_test, y_train, y_test = train_test_split(X_opt, y, test_size=0.2, random_state=0)
+opt_regressor = LinearRegression()
+opt_regressor.fit(X_train, y_train)
+y_pred_opt = opt_regressor.predict(X_test)
+score = opt_regressor.score(X_test, y_test)
+print(f"Score is {score}")
+
+mae_opt = mean_absolute_error(y_test, y_pred_opt)
+mse_opt = mean_squared_error(y_test, y_pred_opt)
+# mae_opt and mse_opt should be lower than original mae and mse
+print('Predicted vs Real errors:', mae_opt, '||', mse_opt) 
